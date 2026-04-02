3330409922e1aef8f7485f44ca2f7ed008aa4eca
nishanshaju22 <nishan.shaju@icloud.com>
2026-04-02 14:31:51 +1100

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -20,7 +20,7 @@ const despatch_api = axios.create({
 	baseURL: 'https://devex.cloud.tcore.network/api',
 	headers: {
 		'Content-Type': 'application/json',
-		"Api-Key": "9cfa8204f71f09d4bc45001e1cf14417f02be7718d142231e8e880add4492b70",
+		"Api-Key": "",
 	},
 	withCredentials: true
 });
@@ -37,7 +37,7 @@ const invoice_api = axios.create({
 	baseURL: 'https://lbhwkjgtm8.execute-api.us-east-1.amazonaws.com',
 	headers: {
 		'Content-Type': 'application/json',
-		"X-API-KEY": "6a818c652b098f5b854ea5b08d88ffc403ebcd67a14b8754b70ec642744d5fcb",
+		"X-API-KEY": "",
 	},
 });
 
